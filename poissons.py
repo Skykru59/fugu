c631a6f2aeb5d1c8db16fcc4ecfa4c209afece1c
bruno5962 <sauvalbruno@gmail.com>
2022-09-27 09:34:58 +0200

    plateau

---
+++ poissons.py
@@ -0,0 +1,22 @@
+'''class grille():
+
+__init__(self, largeur, hauteur)
+self.largeur = 10
+self.hauteur = 8
+self.grille = {[ None for _ in range(largeur)] [for _ in range(hauteur)]}
+    
+print(self)'''
+
+
+grilleJoueur=[[' ' for i in range(8)] for i in range(10)]
+print(grilleJoueur)
+
+
+
+print()
+for i in grilleJoueur:
+
+
+    print(i)
+    
+ 
